83a05501a20ef81c7aba5e0fe2bfa9f472c6ecf5
Gregory Sherrid <gregsherrid@gmail.com>
2016-03-08 20:12:48 -0800

    abstract out Plotter and ZipCodes functionalit; add cluster script for finding clusters

---
+++ cluster.rb
@@ -0,0 +1,127 @@
+require 'csv'
+require 'set'
+require './zipcodes.rb'
+require './plotter.rb'
+
+# Max cluster size, in meters
+CLUSTER_RADIUS = 20000
+DELIMITER = "\t"
+USE_ITEM_WEIGHT = true
+
+input_location_file = ARGV[0]
+raise "Please provide a csv file of ZIP codes" if input_location_file.nil?
+
+output_image_file = ARGV[1] || "./outputs/cluster-output-#{Time.now.to_i}.png"
+
+def main_cluster(input_file, output_file)
+	items = []
+	zips = Set.new
+	CSV.foreach(input_file, headers: true, col_sep: DELIMITER) do |row|
+		location = ZipCodes.lookup(row["ZIP"])
+		if location
+			items << row.to_hash
+			zips << row["ZIP"]
+		end
+	end
+
+	# Build a matrix to lookup any pair of zips
+	matrix = {}
+	zips.each do |zip1|
+		zips.each do |zip2|
+			dist = get_zip_distance(zip1, zip2)
+			key = "#{zip1}x#{zip2}"
+			matrix[key] = dist
+		end
+	end
+
+	clusters = []
+	cluster_centers = []
+
+	# Take out the largest cluster one cluster at a time
+	while items.any?
+
+		# Bulid a hash of zips to the cluster 'size' (by weight or count)
+		zip_to_cluster_size = {}
+		zips.each do |zip1|
+			next if zip_to_cluster_size[zip1]
+			zip_to_cluster_size[zip1] = 0
+
+			# Find remaining items within the cluster radius
+			items.each do |item|
+				dist = get_zip_distance(zip1, item["ZIP"])
+				if dist <= CLUSTER_RADIUS
+					inc = USE_ITEM_WEIGHT ? item["Weight"].to_i : 1
+					zip_to_cluster_size[zip1] += inc
+				end
+			end
+		end
+
+		# Pick the largest zip, take the cluster around it, and remove it from items
+		center_zip = zip_to_cluster_size.to_a.max_by { |pair| pair[1] }[0]
+
+		cluster = items.select do |item|
+			dist = get_zip_distance(center_zip, item["ZIP"])
+			dist <= CLUSTER_RADIUS
+		end
+
+		clusters << cluster
+		cluster_centers << center_zip
+		items -= cluster
+
+		weight = sum_cluster_weight(cluster)
+		puts "#{ items.length } Left : #{ weight } & #{ cluster.length }"
+	end
+
+	max_cluster_weight = clusters.map { |c| sum_cluster_weight(c) }.max
+	max_cluster_count = clusters.map(&:length).max
+
+	plotter = Plotter.new
+	plotter.init_circle
+
+	clusters.length.times do |i|
+		cluster = clusters[i]
+		center_zip = cluster_centers[i]
+
+		location = ZipCodes.lookup(center_zip)
+
+		# Calculate the size of the cluster relative to the max
+		size = if USE_ITEM_WEIGHT			
+			sum_cluster_weight(cluster).to_f / max_cluster_weight
+		else
+			cluster.length / max_cluster_count
+		end
+
+		plotter.place_circle(location, size, i)
+	end
+
+	plotter.save(output_file)
+end
+
+def sum_cluster_weight(cluster)
+	cluster.inject(0) { |sum, item| sum + item["Weight"].to_i }
+end
+
+def get_zip_distance(zip1, zip2)
+	dist = get_lat_long_distance(ZipCodes.lookup(zip1), ZipCodes.lookup(zip2))
+end
+
+# Thanks @Oto Brglez http://stackoverflow.com/questions/12966638/how-to-calculate-the-distance-between-two-gps-coordinates-without-using-google-m
+# Takes inputs: ([lat1, long1], [lat2, long2])
+def get_lat_long_distance(loc1, loc2)
+	rad_per_deg = Math::PI/180  # PI / 180
+	rkm = 6371                  # Earth radius in kilometers
+	rm = rkm * 1000             # Radius in meters
+
+	dlat_rad = (loc2[0]-loc1[0]) * rad_per_deg  # Delta, converted to rad
+	dlon_rad = (loc2[1]-loc1[1]) * rad_per_deg
+
+	lat1_rad, lon1_rad = loc1.map {|i| i * rad_per_deg }
+	lat2_rad, lon2_rad = loc2.map {|i| i * rad_per_deg }
+
+	a = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad/2)**2
+	c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+
+	rm * c # Delta in meters
+end
+
+main_cluster(input_location_file, output_image_file)
--- main.rb
+++ main.rb
@@ -1,66 +1,31 @@
 require 'csv'
-require 'mini_magick'
-
-PIN_SCALE = 15
+require './zipcodes.rb'
+require './plotter.rb'
 
 input_location_file = ARGV[0]
 raise "Please provide a csv file of ZIP codes" if input_location_file.nil?
 output_image_file = ARGV[1] || "./outputs/output-#{Time.now.to_i}.png"
 
-def main(map_template_file, output_file)
-	loc_pairs = us_zip_codes_to_lat_longs(map_template_file)
+def main(zips_file, output_file)
+	loc_pairs = us_zip_codes_to_lat_longs(zips_file)
 	output_map(loc_pairs, output_file)
 end
 
 def output_map(loc_pairs, output_image_file)
-	map_base = MiniMagick::Image.new("./templates/mainland-us-mercator-smaller-green.png")
-	map_width = map_base.width
-	map_height = map_base.height
+	plotter = Plotter.new
+	plotter.init_pin
 
-	pin = MiniMagick::Image.new("./images/blue-mark.png")
-	pin.resize "#{PIN_SCALE}x#{PIN_SCALE}"
-
-	pin_width = pin.width
-	pin_height = pin.height
-
-
-	result = map_base
-
-	loc_pairs.each_with_index do |pair, i|
-		x = scaled_longitude(pair[1], map_width) - pin_width / 2
-		y = scaled_latitude(pair[0], map_height) - pin_height
-		puts "#{i}:   #{x} x #{y}"
-
-		result = result.composite(pin) do |c|
-			c.compose "Over"
-			c.geometry "+#{x}+#{y}"
-		end
+	loc_pairs.each_with_index do |location, i|
+		plotter.place_pin(location, i)
 	end
-	result.write output_image_file
+
+	plotter.save(output_image_file)
 end
-
-def scaled_latitude(lat, scale)
-	south = 24.396308
-	north = 49.384358
-
-	scale - (((lat - south) / (north - south)) * scale).floor
-end
-
-def scaled_longitude(long, scale)
-	west = -124.848974
-	east = -66.885444
-	scale - (((long - east) / (west - east)) * scale).floor
-end
-
 
 def us_zip_codes_to_lat_longs(file)
-	zipcode_to_location = {}
-	CSV.read("./zipcodes.csv").each do |line|
-		zipcode_to_location[line[0]] = [line[3].to_f, line[4].to_f]
-	end
 	locations = []
 	CSV.read(file).each do |a|
-		locations << zipcode_to_location[a[0]]
+		locations << ZipCodes.lookup(a[0])
 	end
 
 	locations.compact
